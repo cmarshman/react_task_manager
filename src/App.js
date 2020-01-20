4a30f9ed99bcf371ddebaf96fc3b7987c865f4c6
Cory Marshman <corymarshman@Corys-Air.attlocal.net>
2020-01-19 19:05:41 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css';
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+
+  }
+
+  markComplete = (id) => {
+    console.log(id)
+  }
+
+  render() {
+    // console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} 
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
